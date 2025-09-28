a37e37501e64afee020a4fd31dc0f4be485c6db2
krishnapriya Shankar <krithikakrishnapriya@gmail.com>
2025-09-28 22:23:12 +0530

    Design creation

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+export default function App(){
+  
+
+
+
+  
+  return(
+
+    <main>
+      <section className="Header">
+        <h1>Assembly : End Game</h1>
+        <p>Guess the word in under 8 attempts to keep the programming world safe from Assembly!</p>
+      </section>
+
+      <section className="Status">
+        <h2> You win</h2>
+        <p>Well done! 🎉</p>
+      </section>
+
+      <section className="Languages"></section>
+
+      <section className="Letters"></section>
+
+      <section className="Keyboard"></section>
+
+      <button className="NewGameButton">New Game</button>
+
+    </main>
+  )
+}
