45b6f6e98cdc79fca2992c4fdb814ceda52908bd
krishnapriya Shankar <krithikakrishnapriya@gmail.com>
2025-10-01 13:43:10 +0530

    Initial App Design Base creation

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,41 @@
+import { useState } from "react"
+import { languages } from "./languages"
+import { wordsList } from "./wordsList"
+
+
 export default function App(){
-  
+
+  // State variables
+  const [word, setWord] = useState("react")
 
 
+  // Static Variables
+  const alphabets = "abcdefghijklmnopqrstuvwxyz".toUpperCase().split("")
+
+ 
+
+
+  const languageChips = languages.map((language,index)=>{
+    const styles ={backgroundColor: language.backgroundColor, color: language.color}
+    return (
+    <span className="LanguageChip" key={index} style={styles}>{language.name}</span>
+  )})
+
+  const letters = word.split("").map((letter, index) => (
+      <div className="Letter" key={index}>{letter.toUpperCase()}</div>
+  ));
+
+  function getRandomWord(){
+    const randomIndex = Math.floor(Math.random() * wordsList.length)
+    const randomWord = wordsList[randomIndex]
+    setWord(randomWord)
+  }
+
+  const keyboard= alphabets.map((letter,index)=>{
+    return(
+      <button className="letterKey" key={index}>{letter}</button>
+    )
+  })
 
   
   return(
@@ -17,13 +51,13 @@ export default function App(){
         <p>Well done! 🎉</p>
       </section>
 
-      <section className="Languages"></section>
+      <section className="Languages">{languageChips}</section>
 
-      <section className="Letters"></section>
+      <section className="Word">{letters}</section>
 
-      <section className="Keyboard"></section>
+      <section className="Keyboard">{keyboard}</section>
 
-      <button className="NewGameButton">New Game</button>
+      <button className="NewGameButton" onClick={getRandomWord}>New Game</button>
 
     </main>
   )
